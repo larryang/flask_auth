dcfb29b39f074fdb58daf5ec788808686f117b76
Larry Yang <2438671+larryang@users.noreply.github.com>
2022-04-18 00:14:40 -0400

    unit test check for request.log
    logged After Request and Before Request to request.log

--- tests/logging_test.py
+++ tests/logging_test.py
@@ -7,3 +7,10 @@ def test_logfile_misc_debug():
     log_dir = os.path.join(test_path, '../app/logs')
     filepath = os.path.join(log_dir, "misc_debug.log")
     assert os.path.isfile(filepath)
+
+def test_logfile_request():
+    """ check if misc_debug.log exists """
+    test_path = os.path.dirname(os.path.abspath(__file__))
+    log_dir = os.path.join(test_path, '../app/logs')
+    filepath = os.path.join(log_dir, "request.log")
+    assert os.path.isfile(filepath)
