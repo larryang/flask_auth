94106bc5bbcbfe0304d72d4a0034e489c7ee8d6d
Larry Yang <2438671+larryang@users.noreply.github.com>
2022-04-17 23:26:14 -0400

    Merge new logger code (#6)
    * logging
    
    * fixing automatic start
    
    * fix
    
    * added some comments
    
    * javascript password validation start
    
    * logging refactor part 1
    
    * logging refactor part 1
    
    * registration password progress bar part 1
    
    * Registration with an Observer
    
    * Registration with an Observer
    
    * added error
    
    * fixed missing logging config
    
    * added SQL logging
    
    * optimized some imports and fixed some logging configuration to write to the correct files.
    
    * fix create-log-folder unit test, cleanup pylint, add logs to .gitignore
    
    * fix capitlization for filename
    
    * add init log directory (before flask startup)
    
    * try again to create log folder
    
    * add logs directory to source control
    
    Co-authored-by: kaw393939 <keith@webizly.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,5 @@
 """This makes the test configuration setup"""
 # pylint: disable=redefined-outer-name
-
 import pytest
 from app import create_app
 
@@ -8,6 +7,7 @@ from app import create_app
 @pytest.fixture()
 def application():
     """This makes the app"""
+
     application = create_app()
     application.config.update({
         "TESTING": True,
