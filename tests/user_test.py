4d4b32a117d3af38d88b2396086b8a8778daecc4
Larry Yang <2438671+larryang@users.noreply.github.com>
2022-04-23 19:10:56 +0000

    fix pytest/pylint issues

--- tests/user_test.py
+++ tests/user_test.py
@@ -1,14 +1,17 @@
+""" test user model/database """
 import logging
 
+#from faker import Faker
 from app import db
 from app.db.models import User, Song
-from faker import Faker
 
-def test_adding_user(application, add_user):
+
+def test_adding_user(application, add_user): # pylint: disable=unused-argument
+    """ test adding a user to database  """
     log = logging.getLogger("myApp")
     with application.app_context():
-        assert db.session.query(User).count() == 1
-        assert db.session.query(Song).count() == 0
+        assert db.session.query(User).count() == 1 # pylint: disable=no-member
+        assert db.session.query(Song).count() == 0 # pylint: disable=no-member
         #showing how to add a record
         #create a record
         #user = User('keith@webizly.com', 'testtest')
@@ -24,23 +27,19 @@ def test_adding_user(application, add_user):
         #asserting that the user retrieved is correct
         assert user.email == 'keith@webizly.com'
         #this is how you get a related record ready for insert
-        user.songs= [Song("test"),Song("test2")]
+        user.songs = [Song("test", "artist1"), Song("test2", "artist2")]
         #commit is what saves the songs
-        db.session.commit()
-        assert db.session.query(Song).count() == 2
+        db.session.commit() # pylint: disable=no-member
+        assert db.session.query(Song).count() == 2 # pylint: disable=no-member
         song1 = Song.query.filter_by(title='test').first()
         assert song1.title == "test"
         #changing the title of the song
         song1.title = "SuperSongTitle"
         #saving the new title of the song
-        db.session.commit()
+        db.session.commit() # pylint: disable=no-member
         song2 = Song.query.filter_by(title='SuperSongTitle').first()
         assert song2.title == "SuperSongTitle"
         #checking cascade delete
-        db.session.delete(user)
-        assert db.session.query(User).count() == 0
-        assert db.session.query(Song).count() == 0
-
-
-
-
+        db.session.delete(user) # pylint: disable=no-member
+        assert db.session.query(User).count() == 0 # pylint: disable=no-member
+        assert db.session.query(Song).count() == 0 # pylint: disable=no-member
