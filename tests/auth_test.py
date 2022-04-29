d2f09f8afbadd45e2daf6f655c0b9f28e067f9f9
Larry Yang <2438671+larryang@users.noreply.github.com>
2022-04-28 20:53:26 -0400

    unit test for logged in user to access /daschboard

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -55,3 +55,17 @@ def test_login(client, add_db_user_fixture):
     # if login, redirect to /dashboard
     assert resp.status_code == 200
     assert b'<h2>Dashboard</h2>' in resp.data
+
+
+def test_dashboard(application, add_db_user_fixture):
+    """ POST to dashboard as logged in user """
+    # pylint: disable=redefined-outer-name
+    user = add_db_user_fixture
+
+    with application.test_client(user=user) as client:
+        resp = client.get('dashboard')
+
+    # check if successful at getting /dashboard
+    assert resp.status_code == 200
+    assert b'<h2>Dashboard</h2>' in resp.data
+    assert b'<p>Welcome: testuser@test.com</p>' in resp.data
