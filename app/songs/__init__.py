395f287be275037fe3cd4a7b25aabee2e9f212d6
Larry Yang <2438671+larryang@users.noreply.github.com>
2022-05-01 00:03:58 -0400

    fix upload multiple times bug, and check for POST

--- app/songs/__init__.py
+++ app/songs/__init__.py
@@ -3,7 +3,7 @@ import csv
 import logging
 import os
 
-from flask import Blueprint, render_template, abort, url_for,current_app
+from flask import Blueprint, render_template, abort, url_for,current_app, request
 from flask_login import current_user, login_required
 from jinja2 import TemplateNotFound
 from werkzeug.utils import secure_filename, redirect
@@ -33,31 +33,32 @@ def songs_upload():
     log = logging.getLogger("upload_songs")
     form = csv_upload()
 
-    if form.validate_on_submit():
-        filename = secure_filename(form.file.data.filename)
-        filepath = os.path.join(current_app.config['UPLOAD_FOLDER'], filename)
-        form.file.data.save(filepath)
-        list_of_songs = []
-        with open(filepath, encoding="utf-8" ) as file:
-            log.info("[%s] opened and parsing filepath:[%s]",
-                 current_user, filepath)
-            csv_file = csv.DictReader(file)
-           
-            # TODO check CSV for proper headers
-            csv_header = Song.csv_headers()
-            for row in csv_file:
-                title = row[csv_header[0]]
-                artist = row[csv_header[1]]
-                year = row[csv_header[2]]
-                genre = row[csv_header[3]] 
-                list_of_songs.append(Song(title, artist, year, genre))
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            filename = secure_filename(form.file.data.filename)
+            filepath = os.path.join(current_app.config['UPLOAD_FOLDER'], filename)
+            form.file.data.save(filepath)
+            list_of_songs = []
+            with open(filepath, encoding="utf-8" ) as file:
+                log.info("[%s] opened and parsing filepath:[%s]",
+                    current_user, filepath)
+                csv_file = csv.DictReader(file)
+            
+                # TODO check CSV for proper headers
+                csv_header = Song.csv_headers()
+                for row in csv_file:
+                    title = row[csv_header[0]]
+                    artist = row[csv_header[1]]
+                    year = row[csv_header[2]]
+                    genre = row[csv_header[3]] 
+                    list_of_songs.append(Song(title, artist, year, genre))
 
-        current_user.songs = list_of_songs # pylint: disable=assigning-non-slot
-        db.session.commit()
+            current_user.songs += list_of_songs # pylint: disable=assigning-non-slot
+            db.session.commit()
 
-        return redirect(url_for('songs.songs_browse'))
-    else:
-        log.info("form failed validation")
+            return redirect(url_for('songs.songs_browse'))
+        else:
+            log.info("form failed validation")
 
     try:
         return render_template('upload.html', form=form)
